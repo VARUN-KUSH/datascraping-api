6480fbbef7d5510d25b36b47131523713ae374ca
VARUN-KUSH <shiva92637@gmail.com>
2024-05-09 14:19:37 +0530

    fixed error

--- api/index.js
+++ api/index.js
@@ -1,6 +1,11 @@
 import express from "express"
 import cors from "cors"
+import dotenv from "dotenv"
+dotenv.config({
+    path: './.env'
+})
 
+const PORT = 9000;
 
 const app = express()
 
@@ -15,12 +20,15 @@ app.use(express.static("public"))
 
 
 
-import homeRouter from './routes/home.routes.js'
+import homeRouter from '../routes/home.routes.js'
 
 
 
 app.use("/api/v1/home", homeRouter)
 
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`⚙️ Server is running at port : ${process.env.PORT || PORT}`);
+})
 
 // http://localhost:8000/api/v1/home/
 
