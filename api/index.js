e1419c392c0ddc935bb3eec4af8dc2c4a2c0ee41
VARUN-KUSH <shiva92637@gmail.com>
2024-05-09 18:59:43 +0530

    cors added

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 import express from "express"
-
+import dotenv from "dotenv"
+dotenv.config({
+    path: './.env'
+})
 
 const PORT = 9000;
 
@@ -10,7 +13,10 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("public"))
-
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
 
 
 import homeRouter from '../routes/home.routes.js'
